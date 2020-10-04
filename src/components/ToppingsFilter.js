74d22962c560067a2d07b5fcaddfed2e24dececc
Hamdan Prakoso <hamdan.prakoso007@gmail.com>
2020-10-04 19:04:51 +0700

    Create fluid images and topping filters

---
+++ src/components/ToppingsFilter.js
@@ -0,0 +1,80 @@
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
+
+const ToppingStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+  }
+`;
+
+function countPizzasInToppings(pizzas) {
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      const existing = acc[topping.id];
+      if (existing) {
+        existing.count += 1;
+      } else {
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+  const sorted = Object.values(counts).sort((a, b) => b.count - a.count);
+  return sorted;
+}
+
+export default function ToppingsFilter() {
+  const { toppings, pizzas } = useStaticQuery(graphql`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+          vegetarian
+        }
+      }
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            name
+            id
+          }
+        }
+      }
+    }
+  `);
+  const toppingWithCounts = countPizzasInToppings(pizzas.nodes);
+  return (
+    <ToppingStyles>
+      {toppingWithCounts.map((topping) => (
+        <Link key={topping.id} to={`/topping/${topping.name}`}>
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingStyles>
+  );
+}
